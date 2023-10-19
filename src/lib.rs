4ab1b208a3ad6d35dbabb4b68dc403bd0e8c4c72
vablings <47798035+vablings@users.noreply.github.com>
2023-10-19 09:28:17 +0100

    move Entry into impl and actually make it accesible (oops)

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@ impl fmt::Display for FileTreeError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             FileTreeError::SymlinkError(err) => write!(f, "Symlink found {err}"),
-            FileTreeError::UnknownEntryType => write!(f, "Entry is not a File, Folder or Symlink"),
+            FileTreeError::UnknownEntryType => write!(f, ""),
             FileTreeError::FilePathError => write!(f, "Failed to coerce &Path into String"),
         }
     }
@@ -32,40 +32,40 @@ enum EntryKind {
     Folder(Vec<Entry>),
 }
 
-fn read_entry(path: &Path) -> Result<Entry> {
-    let name = path
-        .file_name()
-        .ok_or(FileTreeError::FilePathError)?
-        .to_str()
-        .ok_or(FileTreeError::FilePathError)?
-        .to_string();
+impl Entry {
+    pub fn read_entry(path: &Path) -> Result<Self> {
+        let name = path
+            .file_name()
+            .ok_or(FileTreeError::FilePathError)?
+            .to_str()
+            .ok_or(FileTreeError::FilePathError)?
+            .to_string();
 
-    let ty = fs::metadata(path).unwrap().file_type();
-    if ty.is_file() {
-        Ok(Entry {
-            name,
-            kind: EntryKind::File,
-        })
-    } else if ty.is_dir() {
-        Ok(Entry {
-            name,
-            kind: EntryKind::Folder(read_folder(path)?),
-        })
-    } else if ty.is_symlink() {
-        Err(FileTreeError::SymlinkError(name))
-    } else {
-        Err(FileTreeError::UnknownEntryType)
+        let ty = fs::metadata(path).unwrap().file_type();
+        if ty.is_file() {
+            Ok(Entry {
+                name,
+                kind: EntryKind::File,
+            })
+        } else if ty.is_dir() {
+            Ok(Entry {
+                name,
+                kind: EntryKind::Folder(Entry::read_folder(path)?),
+            })
+        } else if ty.is_symlink() {
+            Err(FileTreeError::SymlinkError(name))
+        } else {
+            Err(FileTreeError::UnknownEntryType)
+        }
     }
+
+    fn read_folder<P: AsRef<Path>>(path: P) -> Result<Vec<Entry>> {
+        let mut out = Vec::new();
+        for entry in fs::read_dir(path).unwrap() {
+            let entry = entry.unwrap();
+            out.push(Entry::read_entry(&entry.path())?);
+        }
+        Ok(out)
+    } 
 }
-
-fn read_folder<P: AsRef<Path>>(path: P) -> Result<Vec<Entry>> {
-    let mut out = Vec::new();
-    for entry in fs::read_dir(path).unwrap() {
-        let entry = entry.unwrap();
-        out.push(read_entry(&entry.path())?);
-    }
-    Ok(out)
-}
-
-
 
